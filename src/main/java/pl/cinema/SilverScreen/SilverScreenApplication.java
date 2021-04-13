27275346211b784869dffae92c285a4f79c5ac10
OlekKhom <olekkhomenko90@gmail.com>
2021-04-13 19:45:19 +0200

    Initial commit

---
+++ src/main/java/pl/cinema/SilverScreen/SilverScreenApplication.java
@@ -0,0 +1,13 @@
+package pl.cinema.SilverScreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverScreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilverScreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/cinema/SilverScreen/SilverScreenApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cinema.SilverScreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverScreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
