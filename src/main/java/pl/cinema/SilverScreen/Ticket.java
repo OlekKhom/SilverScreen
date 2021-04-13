98ca5392c74e8ca144a9d451cacf33e40ad16378
Lenovo <vitali10.09.1988@gmail.com>
2021-04-13 20:00:32 +0200

    Create class Ticket

---
+++ src/main/java/pl/cinema/SilverScreen/Ticket.java
@@ -0,0 +1,12 @@
+package pl.cinema.SilverScreen;
+
+public class Ticket {
+
+    private int number;
+    private double price;
+
+    public Ticket(int number, double price) {
+        this.number = number;
+        this.price = price;
+    }
+}
