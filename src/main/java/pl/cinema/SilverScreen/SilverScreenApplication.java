a0d959634518c735bb50ad8a2cfc7666e3a05be9
OlekKhom <olekkhomenko90@gmail.com>
2021-04-14 20:19:45 +0200

    create class Films and MoviesRoom

--- src/main/java/pl/cinema/SilverScreen/SilverScreenApplication.java
+++ src/main/java/pl/cinema/SilverScreen/SilverScreenApplication.java
@@ -12,11 +12,20 @@ public class SilverScreenApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SilverScreenApplication.class, args);
 	}
+
 	@Autowired
 	private TicketRepository ticketRepository;
+
+	@Autowired
+	private FilmRepository filmRepository;
+
+	@Autowired
+	private MoviesRoomRepository moviesRoomRepository;
 
 	@PostConstruct
 	public void test(){
 		ticketRepository.save(new Ticket(0,10, 23.5));
+		filmRepository.save(new Film(0,"Man X", "Hugh Michael Jackman", 2019, 107));
+		moviesRoomRepository.save(new MoviesRoom(0, 2, 48, "Beats 6D"));
 	}
 }
