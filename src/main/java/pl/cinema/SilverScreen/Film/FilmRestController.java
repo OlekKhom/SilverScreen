371fe4a0a444bbaf7ebcb150699491750babd872
OlekKhom <olekkhomenko90@gmail.com>
2021-04-20 20:05:43 +0200

    createFilmController

---
+++ src/main/java/pl/cinema/SilverScreen/Film/FilmRestController.java
@@ -0,0 +1,22 @@
+package pl.cinema.SilverScreen.Film;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class FilmRestController {
+
+    @Autowired
+    FilmRepository filmRepository;
+
+    @PutMapping("/film")
+    public String filmGet() {
+        return "Terminator";
+    }
+
+    @PostMapping("/film/{name}")
+    public String filmPost (@PathVariable String name, @RequestParam String mainActor, @RequestParam int releaseYear){
+        return name + " " + mainActor + " " + releaseYear;
+    }
+
+}
